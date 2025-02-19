395bef752dc2141c577493ba997af8f7238c7823
Lineyier <lenovorus22@gmail.com>
2025-02-19 09:42:22 +0700

    first committ

--- gitinit.cpp
+++ gitinit.cpp
@@ -1,1 +1,10 @@
-sasas
+#include <iostream>
+
+int main(int, char**){
+    setlocale(LC_ALL,"Russian");
+    std::cout << "Разбудите Романыча плиз\n";
+   int a = 121000;
+    int b = 234222;
+    std::cout << "Ответ: "<< a + b;
+    
+}
